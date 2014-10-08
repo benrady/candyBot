03d8c31861739b985038127ccd34a14e7999efdb
Ben Rady <benrady@gmail.com>
2014-10-07 21:03:32 -0500

    Added dispenseCandy function

--- candyBot/candyBot.ino
+++ candyBot/candyBot.ino
@@ -1,4 +1,8 @@
 #include "pitches.h"
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
+
 const int REST = 0;
 
 // notes in the melody:
@@ -16,13 +20,32 @@ int noteDurations[] = {
 };
 
 const int buttonPin = 2;     
-const int deathLedPin =  12;      
-const int candyLedPin =  11;      
+const int deathLedPin =  5;      
+const int candyLedPin =  6;
+const int speakerPin =  7;
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+
+// Connect a stepper motor with 200 steps per revolution (1.8 degree)
+// to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
+
 void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Stepper test!");
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  
+  myMotor->setSpeed(10);  // 10 rpm   
+  
   // initialize the LED pin as an output:
   pinMode(deathLedPin, OUTPUT);
   pinMode(candyLedPin, OUTPUT);  
@@ -38,7 +61,7 @@ void playFanfare() {
       delay(1000/16);
     } else {
       int noteDuration = 1000/noteDurations[thisNote];
-      tone(8, melody[thisNote],noteDuration);
+      tone(speakerPin, melody[thisNote],noteDuration);
   
       // to distinguish the notes, set a minimum time between them.
       // the note's duration + 30% seems to work well:
@@ -46,7 +69,7 @@ void playFanfare() {
       delay(pauseBetweenNotes);
     }
     // stop the tone playing:
-    noTone(8);
+    noTone(speakerPin);
   }
 }
 
@@ -57,19 +80,39 @@ void playGame() {
     time = time + (time / 10);
     if (candy) {
       digitalWrite(deathLedPin, HIGH);
-      tone(8, NOTE_E5, 250);
+      tone(speakerPin, NOTE_E5, 250);
     } else {
       digitalWrite(candyLedPin, HIGH); 
-      tone(8, NOTE_G5, 250);
+      tone(speakerPin, NOTE_G5, 250);
     }
     candy = !candy;    
     delay(time);
     digitalWrite(deathLedPin, LOW); 
     digitalWrite(candyLedPin, LOW);
-    noTone(8);
+    noTone(speakerPin);
   }
   digitalWrite(candyLedPin, HIGH);
 }
+
+
+void dispenseCandy() {
+  Serial.println("Single coil steps");
+  myMotor->step(100, FORWARD, SINGLE); 
+  myMotor->step(100, BACKWARD, SINGLE); 
+
+  Serial.println("Double coil steps");
+  myMotor->step(100, FORWARD, DOUBLE); 
+  myMotor->step(100, BACKWARD, DOUBLE);
+  
+  Serial.println("Interleave coil steps");
+  myMotor->step(100, FORWARD, INTERLEAVE); 
+  myMotor->step(100, BACKWARD, INTERLEAVE); 
+  
+  Serial.println("Microstep steps");
+  myMotor->step(50, FORWARD, MICROSTEP); 
+  myMotor->step(50, BACKWARD, MICROSTEP);
+}
+
 
 void loop(){
   // read the state of the pushbutton value:
@@ -83,7 +126,7 @@ void loop(){
   else {
     playGame();        
     playFanfare();
-    // Dispense Candy
+    dispenseCandy();
   }
 }
 
